4df3f4329300c3e7cd0c39f3196e0bd0f5de2f5c
Ben Jen <benjendrzejak@quickenloans.com>
2019-01-27 15:59:43 -0500

    Finished all but RegistrationSuccess

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Lab21.DAL;
+using Lab21.Models;
+
+namespace Lab21.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly PersonContext _context = new PersonContext();
+        // GET: Person
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Person newPerson)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = "The information you entered is not valid.";
+                return View();
+            }
+
+            _context.Person.Add(newPerson);
+            _context.SaveChanges();
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        public ActionResult RegistrationSuccess()
+        {
+            return View();
+        }
+    }
+}
---
+++ DAL/PersonContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Lab21.Models;
+
+namespace Lab21.DAL
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("PersonContext")
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
